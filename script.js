3fa257adfb8788db05755016c508a9802236ba3b
Sitora Urazova <sitoraurazova@gmail.com>
2018-09-23 21:52:34 +0000

    Reg Ex: Match all numbers

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //FreeCodeCamp Regular Expressions 
-//RegEx: Match everything but letters and numbers 
+//RegEx: Match all numbers
 
-let quoteSample = "The five boxing wizards jump quickly.";
-let nonAlphabetRegex = /\W/g; // Change this line
-let result = quoteSample.match(nonAlphabetRegex).length;
+let numString = "Your sandwich will be $5.00";
+let numRegex = /\d/g; // Change this line
+let result = numString.match(numRegex).length;
